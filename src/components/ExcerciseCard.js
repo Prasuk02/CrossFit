9daa306b311ca5ed433f31ff44224921cf1e8a42
Prasuk Jain <prasukj02@gmail.com>
2023-07-29 17:55:08 +0530

    fixed problem in excerise path

--- src/components/ExcerciseCard.js
+++ src/components/ExcerciseCard.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 function ExcerciseCard({id, name, bodyPart, equipment, gifUrl, target}){
     return(
         <>
-            <Box onClick={window.scrollTo({top: 0, behavior: 'smooth'})} width="20.5%" style={{backgroundColor: 'white', margin: '0px 0px 20px 0px', borderRadius: '10px', padding: '15px', border: '1px solid #cccccc'}}>
+            <Box width="20.5%" style={{backgroundColor: 'white', margin: '0px 0px 20px 0px', borderRadius: '10px', padding: '15px', border: '1px solid #cccccc'}}>
                 <Link to={`/excercise/${id}`} style={{textDecoration: 'none'}}>
                     <img src={gifUrl} width="100%" loading='lazy' alt='excercise-gif'/>
                     <Stack direction='row' alignItems='center'>
